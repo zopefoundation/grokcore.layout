e21c5edb61cd12a27f0e2fa629e4a363118ae69d
Jan-Wijbrand Kolman <jkolman@minddistrict.com>
2018-01-17 13:41:13 +0100

    Back to development: 3.0.3

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ test_requires = [
 
 setup(
     name='grokcore.layout',
-    version='3.0.2',
+    version='3.0.3.dev0',
     description="A layout component package for zope3 and Grok.",
     long_description=long_description,
     classifiers=[
